e2b26ba04295066c63334c42024f8cc30e971f70
Elliot Kim <elliotkim916@gmail.com>
2018-02-27 19:47:42 -0800

    able to make get, post, put, and delete requests

--- routes/logs.js
+++ routes/logs.js
@@ -3,31 +3,85 @@
 const router = require('express').Router();
 const jsonParser = require('body-parser').json();
 
-const logs = [
-    {
-        quality: 'Good',
-        description: 'Slept a full 8 hours with no interruptions!',
-        created: 'February 24, 2018'
-    },
-    {
-        quality: 'Bad',
-        description: 'Barely got any sleep, and it was hard to fall asleep because of noisy neighbors!',
-        created: 'February 25, 2018'
-    },
-    {
-        quality: 'Good',
-        description: 'Sleep was amazing! Felt so energized when I woke up.',
-        created: 'February 26, 2018'
-    }
-];
+const {SleepLog} = require('../models/logs');
 
 router.get('/', function(req, res) {
-    res.send(logs);
+    SleepLog
+        .find()
+        .then(logs => {
+            res.json(logs);
+        });
+});
+
+router.get('/:id', function(req, res) {
+    SleepLog
+        .findById(req.params.id)
+        .then(logs => {
+            res.json(logs)
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Something went horribly awry'});
+        });
 });
 
 router.post('/', jsonParser, function(req, res) {
-    logs.push(req.body);
-    res.json(req.body).status(200);
+    // ensure quality and description are in the request body
+    const requiredFields = ['quality', 'description'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    
+    SleepLog
+        .create({
+            quality: req.body.quality,
+            description: req.body.description,
+        }).then(log => res.status(201).json(log))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Something went wrong..'});
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    SleepLog
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).json({message: 'Success'});
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Something went terribly wrong..'});
+        });
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    if (!(req.params.id && req.body.id === req.body.id)) {
+        res.status(400).json({
+            error: 'Request path id and request body id values must match'
+        });
+    }
+
+    const updated = {};
+    const updatableFields = ['quality', 'description'];
+    updatableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
+
+    SleepLog   
+        .findByIdAndUpdate(req.params.id, { $set: updated}, {new: true})
+        .then(updatedPost => {
+            res.status(204).end()
+        }).catch(err => {
+            res.status(500).json({message: 'Something went wrong..'});
+        });
 });
 
 module.exports = router;
