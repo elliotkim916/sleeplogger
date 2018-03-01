d8d4fd76fc53d46dc43c5772ebe13fd394b19469
Elliot Kim <elliotkim916@gmail.com>
2018-02-28 18:19:32 -0800

    able to delete sleep log posts from the DOM

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@
 
 const SLEEPLOG_ENDPOINT = '/api/logs';
 
+// gets our sample data
 function getSleepLogs() {
     $.get(SLEEPLOG_ENDPOINT, renderSleepLog);
 }
@@ -24,6 +25,52 @@ function postSleepLog(path, sleepQuality, sleepLogText, callback) {
     });
 }
 
+function putSleepLog(path, newQuality, newDescription, callback) {
+    $.ajax({
+        type: 'PUT',
+        url: path,
+        contentType: 'application/json',
+        data: JSON.stringify({
+            quality: newQuality,
+            description: newDescription
+        }),
+        success: callback,
+        error: function(err) {
+            console.info('There is an error!');
+            console.error(err);
+        }    
+    });
+}
+
+function updateEventListener() {
+    $('.sleep-logs-list').on('click', '.update-log', function() {
+        console.log('clicked');
+        const toUpdateLogInput = $(this).closest('.log-container');
+        const newQuality = toUpdateLogInput.find('.quality').val();
+        const newDescription = toUpdateLogInput.find('.description').val();
+      
+        const toUpdateID = toUpdateLogInput.attr('logID');
+
+    putSleepLog(SLEEPLOG_ENDPOINT + '/' + toUpdateID, newQuality, newDescription, function(response) {
+        console.log(response);
+   
+        });        
+    });
+}
+
+function deleteSleepLog(path, callback) {
+    $.ajax({
+        type: 'DELETE',
+        url: path,
+        contentType: 'application/json',
+        success: callback,
+        error: function(err) {
+            console.info('There is an error');
+            console.error(err);
+        }
+    });
+}
+
 function renderSleepLog(data) {
     const allLogs = [].concat(data || []);
     const sleepLogsHTML = allLogs.map(generateSleepLog).join('');
@@ -31,11 +78,29 @@ function renderSleepLog(data) {
 }
 
 function generateSleepLog(log) {
-    // console.log(log.description);
+    // console.log(log._id);
     return `
-    <li>${log.quality}</li>
-    <li>${log.description}</li>
+    <div class="log-container" logID="${log._id}">
+        <h3>${log.created.slice(0,10)}</h3>
+        <p class="quality">${log.quality}</p>
+        <p class="description">${log.description}</p>
+        <button class="update-log" role="button">Update</button>
+        <button class="delete-log" type="submit" role="button">Delete</button>
+    </div>
     `;
+}
+
+function deleteEventListener() {
+    $('.sleep-logs-list').on('click', '.delete-log', function(event) {
+        console.log('clicked');
+        const deleteLogInput = $(event.currentTarget).closest('.log-container');
+        const deleteLogText = deleteLogInput.val();
+
+        const logID = deleteLogInput.attr('logID');
+        deleteSleepLog(SLEEPLOG_ENDPOINT + '/' + logID, function(response) {
+            window.location.href = '/';
+        });
+    });
 }
 
 function bindEventListeners() {
@@ -52,6 +117,8 @@ $('form').on('submit', function(event) {
 }
 
 $(function() {
+    updateEventListener();
+    deleteEventListener();
     bindEventListeners();
     getSleepLogs();
 });
