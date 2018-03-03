c4cf21910eb001c7e1e9a5faf677c1ed0307f483
Elliot Kim <elliotkim916@gmail.com>
2018-03-02 20:11:44 -0800

    added runServer and closeServer functions for testing

--- server.js
+++ server.js
@@ -11,27 +11,49 @@ const jsonParser = bodyParser.json();
 // all the routing for the sleepLog API lives in this file
 const logsRouter = require('./routes/logs');
 
-const {SleepLog} = require('./models');
+const {PORT, DATABASE_URL} = require('./config');
 
 app.use(express.static('public'));
-
 // We mount the logsRouter at `/api/logs`
 app.use('/api/logs', logsRouter);
 
 app.get('/', (req, res) => {
-    // res.send('hello');
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/', jsonParser, (req, res) => {
-//    SleepLog
-//     .create(req.body)
-//     .then((log) => res.status(201).json(log.serialize()))
-//     .catch(err => {
-//         console.error(err);
-//         res.status(500).json({message: 'Internal server error'});
-//     });
-});
+let server;
+
+function runServer(databaseUrl, port=PORT) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+        })
+        .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+        });
+    });
+ });
+}
+
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+            resolve();
+            });
+        });
+    });
+}
 
 // if server.js is called directly (aka, with `node server.js`), this block runs.
 // but we also export the runServer command so other code (for instance, test code) can start the server as needed.
@@ -39,37 +61,15 @@ app.post('/', jsonParser, (req, res) => {
 // we don't want the server to automatically run, and this conditional block makes that possible.
 
 if (require.main === module) {
-    app.listen(process.env.PORT || 8080, function () {
-        console.info(`App is listening on ${this.address().port}`);
-    });
+    runServer(DATABASE_URL).catch(err => console.error(err));
+    // app.listen(process.env.PORT || 8080, function () {
+    //     mongoose.connect(DATABASE_URL).then(function() {
+    //         console.log('Connected!');
+    //     }).catch(err => {
+    //         console.error(err.message);
+    //     });
+    //     console.info(`App is listening on ${this.address().port}`);
+    // });
 }
 
-
-// let server;
-
-// function runServer() {
-//     const port = process.env.PORT || 8080;
-//     return new Promise((resolve, reject) => {
-//         server = app.listen(port, () => {
-//             console.log(`Your app is listening on port ${port}`);
-//             resolve(server);
-//         }).on('error', err => {
-//             reject(err);
-//         });
-//     });
-// }
-
-// function closeServer() {
-//     return new Promise((resolve, reject) => {
-//         console.log('Closing server!');
-//         server.close(err => {
-//             if(err) {
-//                 reject(err);
-//                 return;
-//             }
-//             resolve();
-//         });
-//     });
-// }
-
-module.exports = {app};
+module.exports = {app, runServer, closeServer};
