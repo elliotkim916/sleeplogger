bfc355a5bfc086dc19d33bab9b8ff30cdee19f75
Elliot Kim <elliotkim916@gmail.com>
2018-03-03 14:41:26 -0800

    completed creating tests for all get, post, put, update, and delete requests

--- test/test-server.js
+++ test/test-server.js
@@ -1,27 +1,175 @@
 'use strict';
 
-const mocha = require('mocha');
+const mongoose = require('mongoose');
+const faker = require('faker');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app} = require('../server');
 const expect = chai.expect;
+const should = chai.should();
+
+const {SleepLog} = require('../models/logs');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
 chai.use(chaiHttp);
 
-describe('sleeplog', function() {
+// deletes db between tests to maintain state between tests
+function tearDownDb() {
+    console.warn('Deleting database!');
+    return mongoose.connection.dropDatabase();
+}
+
+// seeds the db
+function seedSleepLogData() {
+    console.info('Seeding sleep log data');
+    const seedData = [];
+    for (let i=1; i<=5; i++) {
+        seedData.push(generateTestSleepLogData());
+    }
+    return SleepLog.insertMany(seedData);
+}
+
+// generates fake log data
+function generateTestSleepLogData() {
+    return {
+        quality: faker.lorem.words(),
+        description: faker.lorem.paragraph(),
+        created: faker.date.past()
+    }
+}
+
+describe('SleepLogger API Resource', function() {
     before(function() {
-        app.listen(8080);
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    beforeEach(function() {
+        return seedSleepLogData();
+    });
+
+    afterEach(function() {
+        return tearDownDb();
     });
 
     after(function() {
-        app.close;
+        return closeServer();
     });
 
-    it('should return 200', function() {
+describe('GET endpoint', function() {
+    it('should return all sleep log posts', function() {
+        let res;
         return chai.request(app)
-        .get('/')
-        .then(function(res) {
-            expect(res).to.have.status(200);
+            .get('/api/logs')
+            .then(function(_res) {
+                res = _res;
+                expect(res).to.be.status(200);
+                expect(res.body).to.have.lengthOf.at.least(1);
+
+                return SleepLog.count();
+            })
+
+            .then(function(count) {
+                expect(res.body).to.have.lengthOf(count);
+            });
+    });
+
+    let resSleepLogPost;
+    it('should return sleep log post with the right fields', function() {
+        return chai.request(app)
+            .get('/api/logs')
+            .then(function(res) {
+                expect(res).to.be.status(200);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('array');
+                expect(res.body).to.have.lengthOf.at.least(1);
+
+                res.body.forEach(function(post) {
+                    expect(post).to.be.a('object');
+                    expect(post).to.include.keys('quality', 'description', 'created');
+                });
+
+                resSleepLogPost = res.body[0];
+                return SleepLog.findById(resSleepLogPost._id);
+            })
+// checking the values in my sleep log post correspond with those in the db
+            .then(function(post) {
+                expect(resSleepLogPost.quality).to.equal(post.quality);
+                expect(resSleepLogPost.description).to.equal(post.description);
+                // expect(resSleepLogPost.created).to.equal(post.created);
+            });
+    });
+
+    describe('POST endpoint', function() {
+        it('should add a new sleep log post', function() {
+        const newPost = generateTestSleepLogData();
+        
+        return chai.request(app)
+            .post('/api/logs')
+            .send(newPost)
+            .then(function(res) {
+                expect(res).to.be.status(201);
+                expect(res).to.be.json;
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.include.keys('quality', 'description', 'created');
+                expect(res.body.quality).to.equal(newPost.quality);
+                expect(res.body.description).to.equal(newPost.description);
+                return SleepLog.findById(res.body._id);
+            })
+    // we retrieve new post from the db and compare its data to the data we sent over
+        .then(function(post) {
+            expect(post.quality).to.equal(newPost.quality);
+            expect(post.description).to.equal(newPost.description);
+            });
         });
     });
+
+    describe('PUT endpoint', function() {
+        it('should update the fields you send over', function() {
+            const toUpdate = {
+                quality: 'Moderate I suppose',
+                description: 'Neighbors were pretty loud so I had a hard time falling asleep'
+            }
+
+        return SleepLog
+            .findOne()
+            .then(post => {
+                toUpdate._id = post._id;
+        
+        return chai.request(app)
+            .put(`/api/logs/${post._id}`)
+            .send(toUpdate);
+        })
+        .then(res => {
+            expect(res).to.have.status(204);
+            return SleepLog.findById(toUpdate._id);
+        }) 
+    // we retrieve the update post from db and prove the post in db is equal to the updated values we sent over   
+        .then(post => {
+            expect(toUpdate.quality).to.equal(post.quality);
+            expect(toUpdate.description).to.equal(post.description);
+        });
+        });
+    });
+
+    describe('DELETE endpoint', function() {
+        it('should delete a sleep log post', function() {
+            let log;
+            return SleepLog
+                .findOne()
+                .then(function(_log) {
+                    log = _log;
+                    return chai.request(app)
+                    .delete(`/api/logs/${log._id}`)
+                })
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                    return SleepLog.findById(log._id);
+                })
+                .then(function(_log) {
+                    expect(_log).to.be.null;
+                });
+        });
+    });
+});   
 });
